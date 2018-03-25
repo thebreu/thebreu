07ba4b3cd771d42f6cded7f2537669f9fdd1ef8a
Ryan Abreu <dev.breu@gmail.com>
2018-03-25 00:56:47 -0600

    added jquery, navbar, and other goodies

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+/* global $ */
+var state = false;
+
+$( document ).ready(function() {
+
+  $('.projButn').on('click', function(){
+    $('.projButn').css('color', state? 'rgba(255,255,255,0.8)' : "rgba(255,255,255,0.2)");
+    $('.projects').css({'height' : state? '0px' : '35vw', "width" : state? '0px': '35vw'});
+    $('.logo-right').css({"transform" : state? 'translateX(0px)' : "translateX(30vw)"});
+    $('.logo-left').css({"transform" : state? 'translateX(0px)' : "translateX(-30vw)"});
+    $('.decoration').css({'height' : state? '0px' : '65vw', 'width' : state? '0px' : '65vw', 'border-color' : state? 'transparent' : 'rgba(255,255,255,0.8)'});
+    state = !state;
+  });
+
+
+});
