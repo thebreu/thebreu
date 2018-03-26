0a1ebb2ffae04867c68e917be008c7baed58b274
Ryan Abreu <dev.breu@gmail.com>
2018-03-26 11:14:21 -0600

    added images/fuctionality to projects/and nav buttons on projects

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,52 @@
 /* global $ */
-var state = false;
-
+var isOpen = false;
+//var phase = false;
 
 $( document ).ready(function() {
+//OPENS UP THE PORTFOLIO SECTION------------------------------
 
   $('.projButn').on('click', function(){
 
-    $('.projButn').css('color', state? 'rgba(255,255,255,0.8)' : "rgba(255,255,255,0.2)");
-    $('.projects').css({'height' : state? '0px' : '38vw', "width" : state? '0px': '38vw'});
-    $('.logo-right').css({"transform" : state? 'translateX(0px)' : "translateX(30vw)"});
-    $('.logo-left').css({"transform" : state? 'translateX(0px)' : "translateX(-30vw)"});
-    $('.decoration').css({'height' : state? '0px' : '58vw', 'width' : state? '0px' : '58vw', 'border-color' : state? 'transparent' : 'rgba(255,255,255,0.8)'});
-    state = !state;
-  });
-  $('.idea').on('click', function(){
-    $('.viewFade').css({'display' : 'block'});
-    $('.view-content').css({'background-image' : "url('stylesheets/css/imgs/portfolio-idea.png')"});
-  });
-  $('.exit').on('click', function(){
-    $('.viewFade').css({'display' : 'none'});
+    $('.projButn').css('color', isOpen? 'rgba(255,255,255,1)' : "rgba(255,255,255,1)");
+    $('.projects').css({'height' : isOpen? '0px' : '38vw', "width" : isOpen? '0px': '38vw'});
+    $('.logo-right').css({"transform" : isOpen? 'translateX(0px)' : "translateX(30vw)"});
+    $('.arrow-right').css({"transform" : isOpen? 'translateX(0px)' : "translateX(23vw)", 'height' : isOpen? '0px' : '150px', 'width' : isOpen? '0px' : '100px'});
+    $('.logo-left').css({"transform" : isOpen? 'translateX(0px)' : "translateX(-30vw)"});
+    $('.arrow-left').css({"transform" : isOpen? 'translateX(0px)' : "translateX(-23vw)", 'height' : isOpen? '0px' : '150px', 'width' : isOpen? '0px' : '100px'});
+    $('.decoration').css({'height' : isOpen? '0px' : '58vw', 'width' : isOpen? '0px' : '58vw', 'border-color' : isOpen? 'transparent' : 'rgba(255,255,255,0.8)'});
+    $('.read-decoration').css({'height' : isOpen? '0px' : '0vh' , 'width' : isOpen? '0px' : '0vh' , 'border-color' : isOpen? 'transparent' : 'transparent'});
+    $('.about-content').css({'height': '0px', 'width' : '0px'});
+
+    isOpen = !isOpen;
   });
 
+  $('.arrow-right').click(function(){
+    
+    $('.project-content').animate({ "left": "-=525px" }, "slow" );
+  });
+  $('.arrow-left').click(function(){
+    $('.project-content').animate({ "left": "+=525px" }, "slow" );
+  });
+  //OPENS UP A LARGER PREVIEW OF THE PROJECT SELECTED-----------------
+  $('.idea').on('click', function(){
+    $('.viewFade').fadeToggle();
+    $('.view-content').css({'background-image' : $(this).css('background-image')});
+  });
+  //PULLS OUT OF THE PREVIEW MODE----------------------
+  $('.exit').on('click', function(){
+    $('.viewFade').fadeToggle();
+  });
+  //OPENS UP THE ABOUT SECTION-----------------------------------
+
   $('.about').on('click', function(){
-    $('.projects').css({'height' : state? '0px' : '0px', "width" : state? '0px': '0px'});
-    $('.logo-right').css({"transform" : state? 'translateX(0px)' : "translateX(-30vw)"});
-    $('.logo-left').css({"transform" : state? 'translateX(0px)' : "translateX(30vw)"});
-    $('.decoration').css({'height' : state? '0px' : '65vw', 'width' : state? '0px' : '65vw', 'border-color' : state? 'transparent' : 'rgba(255,255,255,0.8)'});
-    state = !state;
+    $('.projects').css({'height' : isOpen? '0px' : '0px', "width" : isOpen? '0px': '0px'});
+    $('.logo-right').css({"transform" : isOpen? 'translateX(0px)' : "translateX(-32vw)"});
+    $('.logo-left').css({"transform" : isOpen? 'translateX(0px)' : "translateX(32vw)"});
+    $('.decoration').css({'height' : isOpen? '0px' : '0px', 'width' : isOpen? '0px' : '0px', 'border-color' : isOpen? 'transparent' : 'transparent'});
+    $('.read-decoration').css({'height' : isOpen? '0px' : '90vh' , 'width' : isOpen? '0px' : '61vw' , 'border-color' : isOpen? 'transparent' : 'rgba(255,255,255,0.8)'});
+    $('.about-content').css({'height' : isOpen? '0px' : '95vh', 'width' : isOpen? '0px' : '50vw'});
+    isOpen = !isOpen;
+
   });
 
 });
